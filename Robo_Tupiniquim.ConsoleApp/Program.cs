22d990aa85c834d3da102f7dc7194778dbd45fed
Gabriel Tavares <gabrielgstell@gmail.com>
2024-03-26 17:01:38 -0300

    Feita a Impressão das dimensões do Array2D na tela

--- Robo_Tupiniquim.ConsoleApp/Program.cs
+++ Robo_Tupiniquim.ConsoleApp/Program.cs
@@ -1,10 +1,47 @@
 ﻿namespace Robo_Tupiniquim.ConsoleApp
 {
+    //Robo Tupiniquim
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int x, y;
+
+            Console.WriteLine("--------------------");
+            Console.WriteLine("| Robo Tupiniquim! |");
+            Console.WriteLine("--------------------\n");
+
+            Console.WriteLine("Tamanho da Área a ser Explorada:");
+            Console.WriteLine("Digite o tamanho da coordenada X");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Digite o tamanho da coordenada Y");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            int[,] array2D = new int[x, y];
+
+
+
+            //Imprimindo na tela o tamanho do Array Bidimensional
+            Console.Write("|");
+            
+            //Contando os giros de Y e imprimindo
+            for (int i = 0; i < y; i++)
+            {
+                //Imprimindo as casas X
+                for (int j = 0; j < x; j++)
+                {
+                    Console.Write(" |");
+                }
+                Console.WriteLine();
+                //Eliminando o simbolo restante(ajustando o tamanho correto)
+                if (i != y-1)
+                {
+                    Console.Write("|");
+                }
+            }
         }
     }
 }
