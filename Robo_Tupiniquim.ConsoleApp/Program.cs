577b609264be80aa5e748fb3bf99bb539365f6f9
Gabriel Tavares <gabrielgstell@gmail.com>
2024-03-26 16:20:58 -0300

    Adicionar arquivos de projeto.

---
+++ Robo_Tupiniquim.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Robo_Tupiniquim.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
